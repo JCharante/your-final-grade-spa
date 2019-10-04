9a1994c9bdd8498ef944adf3159dc5fd2f1f12a4
jcharante <git@jcharante.com>
2019-10-04 12:36:33 -0400

    add fab

--- src/pages/Classes.vue
+++ src/pages/Classes.vue
@@ -13,6 +13,9 @@
                 </q-item>
             </q-list>
         </div>
+        <q-page-sticky position="bottom-right" :offset="[18, 18]">
+            <q-btn fab icon="add" color="accent" />
+        </q-page-sticky>
     </q-page>
 </template>
 
