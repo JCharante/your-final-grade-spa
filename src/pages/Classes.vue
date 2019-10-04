8965e39f15b9ae0ffe3b87f874e4b5af967dfa14
jcharante <git@jcharante.com>
2019-10-04 12:30:18 -0400

    add placeholder text

--- src/pages/Classes.vue
+++ src/pages/Classes.vue
@@ -1,15 +1,39 @@
 <template>
     <q-page>
-        <p>It doesn't look like you have any classes :(</p>
+        <div class="q-pa-md" style="max-width: 350px">
+            <template v-if="true">
+                <p class="text-center" id="it">It doesn't look like you have any classes :(</p>
+                <p class="text-center">Why not add one?</p>
+            </template>
+            <q-list v-else>
+                <q-item clickable>
+                    <q-item-section>
+                        <q-item-label>Chem 1010</q-item-label>
+                    </q-item-section>
+                </q-item>
+            </q-list>
+        </div>
     </q-page>
 </template>
 
 <script>
+    import { mapActions } from 'vuex';
+
     export default {
         name: "Classes",
+        methods: {
+            ...mapActions([
+                'setPageTitle',
+            ]),
+        },
+        mounted() {
+            this.setPageTitle({ name: 'Classes' });
+        },
     };
 </script>
 
 <style scoped>
-
+    #it {
+        margin-top: 100%;
+    }
 </style>
