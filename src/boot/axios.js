befc7c3948b546673c0c9ef2fd14ab664c74c52e
jcharante <git@jcharante.com>
2019-10-07 21:57:23 -0400

    update base url for GCP cloud function

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3001/',
+    baseURL: 'https://us-east4-your-final-grade.cloudfunctions.net/api-service',
 });
 
 export default async ({ Vue }) => {
